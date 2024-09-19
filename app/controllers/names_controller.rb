999bc9e5c3a60f3f71b7fba6d5ce8898276e8dee
Kuniaki Igarashi <kuniaki.igarashi@garnettech373.com>
2024-09-19 11:36:42 +0900

    Remove respond_to do format in NamesController

--- app/controllers/names_controller.rb
+++ app/controllers/names_controller.rb
@@ -13,12 +13,10 @@ class NamesController < ApplicationController
   def create
     @name = UserNameForm.new(model: User.new, **name_params)
 
-    respond_to do |format|
-      if @name.save
-        format.html { redirect_to user_url(@name.user), notice: "User was successfully created." }
-      else
-        format.html { render :new, status: :unprocessable_entity }
-      end
+    if @name.save
+      redirect_to user_url(@name.user), notice: "User was successfully created."
+    else
+      render :new, status: :unprocessable_entity
     end
   end
 
@@ -26,12 +24,10 @@ class NamesController < ApplicationController
   def update
     @name = UserNameForm.new(model: User.find(params[:id]), **name_params)
 
-    respond_to do |format|
-      if @name.save
-        format.html { redirect_to user_url(@name.user), notice: "User was successfully updated." }
-      else
-        format.html { render :edit, status: :unprocessable_entity }
-      end
+    if @name.save
+      redirect_to user_url(@name.user), notice: "User was successfully updated."
+    else
+      render :edit, status: :unprocessable_entity
     end
   end
 
