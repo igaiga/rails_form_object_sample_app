c35a88dc233b4c55cedad3cc3f2bcbeb7c648e44
Kuniaki Igarashi <kuniaki.igarashi@garnettech373.com>
2024-10-08 12:19:50 +0900

    Refactering variable name from name to user_name_form

--- app/controllers/names_controller.rb
+++ app/controllers/names_controller.rb
@@ -1,20 +1,20 @@
 class NamesController < ApplicationController
   # GET /names/new
   def new
-    @name = UserNameForm.new(model: User.new)
+    @user_name_form = UserNameForm.new(model: User.new)
   end
 
   # GET /names/1/edit
   def edit
-    @name = UserNameForm.new(model: User.find(params[:id]))
+    @user_name_form = UserNameForm.new(model: User.find(params[:id]))
   end
 
   # POST /names
   def create
-    @name = UserNameForm.new(model: User.new, **name_params)
+    @user_name_form = UserNameForm.new(model: User.new, **name_params)
 
-    if @name.save
-      redirect_to user_url(@name.user), notice: "User was successfully created."
+    if @user_name_form.save
+      redirect_to user_url(@user_name_form.user), notice: "User was successfully created."
     else
       render :new, status: :unprocessable_entity
     end
@@ -22,10 +22,10 @@ class NamesController < ApplicationController
 
   # PATCH/PUT /names/1
   def update
-    @name = UserNameForm.new(model: User.find(params[:id]), **name_params)
+    @user_name_form = UserNameForm.new(model: User.find(params[:id]), **name_params)
 
-    if @name.save
-      redirect_to user_url(@name.user), notice: "User was successfully updated."
+    if @user_name_form.save
+      redirect_to user_url(@user_name_form.user), notice: "User was successfully updated."
     else
       render :edit, status: :unprocessable_entity
     end
